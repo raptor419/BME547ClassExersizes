bcb90070b9aa04189cb9ea4bd2342f097e1db3b3
Harsh Bandhey <30626026+raptor419@users.noreply.github.com>
2022-01-19 17:34:56 -0500

    Add check HDL feature

--- code.py
+++ code.py
@@ -1,12 +1,35 @@
 def take_input():
-    inp = input("Enter your input")
-    return inp
+    try:
+        inp = int(input("Enter your input"))
+        return inp
+    except:
+        print("Invalid Input, Enter again")
+        return take_input()
 
+def check_HDL(hdl):
+    if hdl>=60:
+        print("Normal")
+    elif hdl>=40:
+        print("Borderline Low")
+    elif hdl<40:
+        print("Low")
+        
+def driver(what):
+    print("Enter",what)
+    hdl = take_input()
+    if what=="HDL":
+        check_HDL(hdl)
+    
 def interface():
     while True:
         print("My Program")
         print("Options:")
+        print("1 - Check HDL")
         print("9 - Quit")
         choice = input("Enter your choice: ")
         if choice=='9':
-            return choice
+            return
+        elif choice=='1':
+            driver("HDL")
+        else:
+            print("Invalid Option, Try again")
