34988975c33f14c1e9227c90597cd9747fe5bc21
Harsh Bandhey <30626026+raptor419@users.noreply.github.com>
2022-01-31 16:41:04 -0500

    Update test_inclass2.py

--- test_inclass2.py
+++ test_inclass2.py
@@ -1,5 +1,5 @@
 def test_function():
     from inclass2 import function
-    x,y,x_new = (0,0),(2,2),1
-    answer = function(x,y,x_new)
+    a,b,x_new = (0,0),(2,2),1
+    answer = function(a,b,x_new)
     assert(answer==1)
